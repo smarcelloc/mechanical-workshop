13baae02315555e9afa4c1983468fc87dbafc0d6
Marcello Silvério <smarcelloc@gmail.com>
2023-03-14 21:11:31 -0300

    add content sidebar

--- resources/layouts/main/MainLayout.vue
+++ resources/layouts/main/MainLayout.vue
@@ -1,6 +1,5 @@
 <template>
     <q-layout view="hHh lpR fFf">
-
         <q-header bordered class="bg-white text-dark">
             <q-toolbar>
                 <q-btn dense flat round icon="menu" class="q-mr-sm" @click="drawerOpen = !drawerOpen" />
@@ -14,19 +13,19 @@
 
         <q-drawer show-if-above v-model="drawerOpen" side="left" bordered :mini="drawerMini" @mouseover="drawerMini = false"
             @mouseout="drawerMini = true" mini-to-overlay :width="200">
-            <!-- drawer content -->
+            <sidebar-content />
         </q-drawer>
 
         <q-page-container>
             <router-view />
         </q-page-container>
-
     </q-layout>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue';
 import MenuProfile from './MenuProfile.vue';
+import SidebarContent from './SidebarContent.vue';
 
 const drawerOpen = ref(false);
 const drawerMini = ref(true);
---
+++ resources/layouts/main/SidebarContent.vue
@@ -0,0 +1,81 @@
+<template>
+    <q-scroll-area class="fit">
+        <q-list padding>
+            <q-item clickable v-ripple>
+                <q-item-section avatar>
+                    <q-icon name="space_dashboard" />
+                </q-item-section>
+                <q-item-section>Dashboard</q-item-section>
+            </q-item>
+
+            <q-separator />
+
+            <q-item clickable v-ripple>
+                <q-item-section avatar>
+                    <q-icon name="forklift" />
+                </q-item-section>
+                <q-item-section>Suppliers</q-item-section>
+            </q-item>
+
+            <q-item clickable v-ripple>
+                <q-item-section avatar>
+                    <q-icon name="precision_manufacturing" />
+                </q-item-section>
+                <q-item-section>Products</q-item-section>
+            </q-item>
+
+            <q-separator />
+
+            <q-item clickable v-ripple>
+                <q-item-section avatar>
+                    <q-icon name="group" />
+                </q-item-section>
+                <q-item-section>Clients</q-item-section>
+            </q-item>
+
+            <q-item clickable v-ripple>
+                <q-item-section avatar>
+                    <q-icon name="directions_car" />
+                </q-item-section>
+                <q-item-section>Vehicles</q-item-section>
+            </q-item>
+
+            <q-item clickable v-ripple>
+                <q-item-section avatar>
+                    <q-icon name="shopping_bag" />
+                </q-item-section>
+                <q-item-section>Order</q-item-section>
+            </q-item>
+
+            <q-separator />
+
+            <q-item clickable v-ripple>
+                <q-item-section avatar>
+                    <q-icon name="car_repair" />
+                </q-item-section>
+                <q-item-section>Services</q-item-section>
+            </q-item>
+
+            <q-item clickable v-ripple>
+                <q-item-section avatar>
+                    <q-icon name="store" />
+                </q-item-section>
+                <q-item-section>Workshops</q-item-section>
+            </q-item>
+
+            <q-item clickable v-ripple>
+                <q-item-section avatar>
+                    <q-icon name="person" />
+                </q-item-section>
+                <q-item-section>Users</q-item-section>
+            </q-item>
+
+            <q-item clickable v-ripple>
+                <q-item-section avatar>
+                    <q-icon name="remove_red_eye" />
+                </q-item-section>
+                <q-item-section>Audits</q-item-section>
+            </q-item>
+        </q-list>
+    </q-scroll-area>
+</template>
